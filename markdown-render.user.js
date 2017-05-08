155477191ab2f3ba1c1ca7cea4ad5139c36d3675
Roy Ivy III <rivy.dev@gmail.com>
2017-05-08 17:55:49 -0500

    wip ~ load_assets() with global_eval()

--- markdown-render.user.js
+++ markdown-render.user.js
@@ -79,6 +79,18 @@ function getScripts(scripts) {
     });
 }
 
+// ref: https://community.oracle.com/blogs/driscoll/2009/09/08/eval-javascript-global-context @@ http://archive.is/qy9fL
+var globalEval = function globalEval(src) {
+    if (window.execScript) {
+        window.execScript(src);
+        return;
+    }
+    var fn = function() {
+        window.eval.call(window,src);
+    };
+    fn();
+};
+
 function get_raw_html( uri, timeout ){ // ( {array}, {int} ) : {$.Deffered}
 // Firefox misinterprets non-HTML (non .htm/.html extension) files as HTML if they contain initial HTML tags and irretrievably alters the text ... this replaces the body content with text equivalent to chrome's interpretation
 // NOTE: no perceptable speed difference when using this on a machine with an SSD ... test, looking at network timing/speed, esp. for regular HDs
@@ -98,6 +110,40 @@ retVal = $.ajax( uri, { cache: true, dataType: 'text', timeout: timeout } )
 return retVal;
 }
 
+function load_asset( uri, timeout, optional ) { // ( {array} [, {int}timeout=0] [, {bool}optional=false] ) => {$.Deferred}
+/**
+ * load assets in parallel, insert/initialize results *in order* within the document
+ * @param {array} : an array of script uris, loaded asynchronously, but placed into the file in the given order
+ * @param {int}   : a timeout for download failure (default == 0 (aka, no timeout))
+ * @returns {$.Deferred}
+ * ref: (based on) https://stackoverflow.com/questions/9711160/jquery-load-scripts-in-order/19777866#19777866 @@ https://archive.is/yt1su
+ * ref: (based on) https://gist.github.com/rivy/5f1bd5225d4ee315a8d7f3c89986600f from https://gist.github.com/ngryman/7309432
+ * ref: [jqXHR ~ .done/.fail/.always/.then argument documentation] http://api.jquery.com/jQuery.ajax/#jqXHR
+ */
+// CSS has order dependence (for rules with equivalent specificity); this function places the CSS in the specified order, creating determinate content for the document
+    timeout = ((timeout !== null) && (timeout >= 0)) ?  timeout : 2 * 1000/* ms */;
+    let _ME = 'get_css()';
+    let style_uris = $.isArray( uri ) ? uri : [ uri ];
+    let requests = style_uris.map( function( style_uri ) {
+        console.log( `${_ME}: initiating AJAX download ("${style_uri}")` );
+        let jqXHR = $.ajax( style_uri, { cache: true, dataType: 'text', timeout: timeout } );
+        jqXHR.uri = style_uri;
+        return jqXHR;
+        });
+
+    return $.when.apply($, requests)
+        .done( function() {
+            Array.prototype.forEach.call( arguments, function( request /* :: [data, textStatus, jqXHR] */, index ) {
+                console.log( `${_ME}: done::${JSON.stringify(request)}:: (${request[2].status}) '${request[2].statusText}' for "${request[2].uri}"` );
+                let css = request[0];
+                $('<style type="text/css" />').html(css).attr('_uri', request[2].uri).attr('_index', index).appendTo('head');
+                });})
+        //.fail( function() { Array.prototype.forEach.call( arguments, function( request /* :: [jqXHR, textStatus, errorThrown] */, index ) { console.log( `${_ME}: FAIL::${JSON.stringify(request)}::` ); throw new Error(`${_ME}: FAIL`); }); })
+        .fail( function() { Array.prototype.forEach.call( arguments, function( request /* :: [jqXHR, textStatus, errorThrown] */, index ) { warn(`loading failed for '${request.uri}'`); console.log( `${_ME}: FAIL::${JSON.stringify(request)}::` ); throw new Error(`${_ME}: FAIL`); }); })
+        //.always( function() { Array.prototype.forEach.call( arguments, function( request /* :: [data | jqXHR, textStatus, jqXHR | errorThrown] */, index ) { console.log( `${_ME}: ALWAYS: '${request[1]}'` ); }); })
+        ;
+}
+
 function load_css( uri, timeout ) { // ( {array}, {int} ) => {$.Deffered}
 /**
  * load CSS in parallel keeping order for document placement
@@ -106,6 +152,7 @@ function load_css( uri, timeout ) { // ( {array}, {int} ) => {$.Deffered}
  * @returns {$.Deferred}
  * ref: (based on) https://stackoverflow.com/questions/9711160/jquery-load-scripts-in-order/19777866#19777866 @@ https://archive.is/yt1su
  * ref: (based on) https://gist.github.com/rivy/5f1bd5225d4ee315a8d7f3c89986600f from https://gist.github.com/ngryman/7309432
+ * ref: [jqXHR ~ .done/.fail/.always/.then argument documentation] http://api.jquery.com/jQuery.ajax/#jqXHR
  */
 // CSS has order dependence (for rules with equivalent specificity); this function places the CSS in the specified order, creating determinate content for the document
     timeout = ((timeout !== null) && (timeout >= 0)) ?  timeout : 2 * 1000/* ms */;
