0afca0069ea824e1ec1ac8b1351287df343e93d5
Roy Ivy III <rivy.dev@gmail.com>
2018-01-01 18:21:01 -0600

    update ~ VERSION

--- markdown-render.user.js
+++ markdown-render.user.js
@@ -3,7 +3,7 @@
 // @namespace   com.houseofivy
 // @description renders markdown files
 //
-// @version     0.143
+// @version     0.145
 // @//updateURL   https://raw.githubusercontent.com/rivy/gms-markdown_viewer.custom-css/master/markdown_viewer.custom-css.user.js
 //
 // file extension: .m(arkdown|kdn?|d(o?wn)?)
