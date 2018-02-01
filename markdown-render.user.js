be507e4adfeb638d0cc230e23d729bff6b9cdc1e
Roy Ivy III <rivy.dev@gmail.com>
2018-01-31 19:35:53 -0600

    change ~ use updated CSS (via CDN)

--- markdown-render.user.js
+++ markdown-render.user.js
@@ -3,7 +3,7 @@
 // @namespace   com.houseofivy
 // @description renders markdown files
 //
-// @version     0.223
+// @version     0.225
 // @//updateURL   https://github.com/rivy/js-user.markdown-render/raw/master/markdown-render.user.js
 //
 // file extension: .m(arkdown|kdn?|d(o?wn)?)
@@ -29,7 +29,7 @@
 
 var script_name = 'markdown-render';
 var script_repo_path = 'rivy/js-user.' + script_name + '/';
-var script_repo_CDN_commit = '12f9c1f1e3c2d9f00c44cd11616ccfede484c74b';
+var script_repo_CDN_commit = '520d0330bbc2202b0ca2b790e9a5a4d8a0c27c2b';
 var script_repo_CDN_base_url = '//cdn.rawgit.com/' + script_repo_path + script_repo_CDN_commit + '/';
 
 GM_registerMenuCommand(`'${script_name}' Settings`, function() {
